263a6af8a86da4d9264e62eeac3736d6fbd2a72f
Georg Kreimer <georg.kreimer@hybris.com>
2015-10-13 16:55:31 +0200

    using yaas config node

--- document.js
+++ document.js
@@ -8,15 +8,14 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         var node = this;
 
-        node.client_id = config.clientId;
-        node.client_secret = config.clientSecret;
-        node.application_id = config.applicationId;
+        node.yaasCredentials = RED.nodes.getNode(config.yaasCredentials);
+
         node.tenant_id = config.tenantId;
         node.document_type = config.documentType;
 
         node.status({fill: "red", shape: "ring", text: "disconnected"});
 
-        oauth2.getClientCredentialsToken(node.client_id, node.client_secret, ['hybris.repository_manage'])
+        oauth2.getClientCredentialsToken(node.yaasCredentials.client_id, node.yaasCredentials.client_secret, ['hybris.repository_manage'])
             .then(function(access_token) {
                 node.status({fill: "green", shape: "dot", text: "connected"});
                 node.access_token = access_token;
@@ -30,7 +29,7 @@ module.exports = function(RED) {
 
             node.log('Writing: ' + msg.payload);
 
-            document.write(node.access_token, node.tenant_id, node.application_id, node.document_type, msg.payload)
+            document.write(node.access_token, node.tenant_id, node.yaasCredentials.application_id, node.document_type, msg.payload)
                 .then(function() {
                     node.log("Message published.");
                 }, console.log);
