526c67a854f77ff1603df2f170f6197e2614989f
Andreas Brain | hybris <andreas.brain@hybris.de>
2015-10-21 12:16:10 +0200

    YaasPubsubSubscribeNoCommitNode

--- pubsub.js
+++ pubsub.js
@@ -24,7 +24,7 @@ module.exports = function(RED) {
             //start inteval polling
             node.intervalID = setInterval(function(){
                 node.log("Polling for " + node.yaasCredentials.application_id + '/' + node.topic);
-                pubsub.readNext(access_token, node.yaasCredentials.application_id, node.topic)
+                pubsub.readNext(access_token, node.yaasCredentials.application_id, node.topic, true)
                 .then(function(evt){
                     if (evt != undefined)
                     {
@@ -50,6 +50,47 @@ module.exports = function(RED) {
     }
 
     RED.nodes.registerType("subscribe",YaasPubsubSubscribeNode);
+
+    function YaasPubsubSubscribeNoCommitNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.yaasCredentials = RED.nodes.getNode(config.yaasCredentials);
+
+        node.topic = config.topic;
+        node.interval = config.interval;
+
+        node.status({fill:"red",shape:"ring",text:"disconnected"});
+
+        //get oauth2 access token
+        oauth2.getClientCredentialsToken(node.yaasCredentials.client_id, node.yaasCredentials.client_secret, [])
+            .then(function(access_token) {
+                node.status({fill:"green",shape:"dot",text:"polling"});
+
+                //start inteval polling
+                node.intervalID = setInterval(function(){
+                    node.log("Polling for " + node.yaasCredentials.application_id + '/' + node.topic);
+                    pubsub.readNext(access_token, node.yaasCredentials.application_id, node.topic, false)
+                        .then(function(evt){
+                            if (evt != undefined)
+                            {
+                                node.send(evt);
+                            }
+                            else
+                            {
+
+                            }
+                        }, console.log);
+                }, node.interval);
+            }, console.log);
+
+        node.on('close', function() {
+            if (node.intervalID) {
+                clearInterval(node.intervalID);
+            }
+        });
+    }
+    RED.nodes.registerType("subscribenocommit",YaasPubsubSubscribeNoCommitNode);
 
     function YaasPubsubPublishNode(config) {
         RED.nodes.createNode(this, config);
