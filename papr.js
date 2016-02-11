50c2f4c95c7dd753158fa5f1c8403f3a6a9c7587
Lars Gregori | hybris <lars.gregori@sap.com>
2016-02-11 11:27:06 +0100

    implemented 'get papr stock'; node.status

--- papr.js
+++ papr.js
@@ -1,9 +1,45 @@
 module.exports = function(RED) {
-   
-    var request = require('request');
+
     var oauth2 = require('./lib/oauth2.js');
     var papr = require('./lib/papr.js');
     var customer = require('./lib/customer.js');
+
+    function PaprGetStock(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.yaasCustomerCredentials = RED.nodes.getNode(config.yaasCustomerCredentials);
+        node.yaasCredentials = RED.nodes.getNode(config.yaasCredentials);
+        node.status({fill:"yellow",shape:"dot",text:"idle"});
+
+        node.on('input', function(msg) {
+
+            node.status({fill:"green",shape:"dot",text:"reading"});
+            var customerToken;
+
+            oauth2.getClientCredentialsToken(node.yaasCredentials.client_id, node.yaasCredentials.client_secret, [])
+            .then(function(authData) {
+
+                return customer.login(authData.tenant, authData.access_token, node.yaasCustomerCredentials.email, node.yaasCustomerCredentials.password)
+                .then(function(token){
+                    console.log('papr customer token: ' + token);
+                    customerToken = token;
+                    return customer.me(authData.tenant, customerToken);
+                })
+                .then(function(customer){
+                    console.log('papr got customer id:  ' + customer.customerNumber);
+                    return papr.getStock(customerToken);
+                });
+            })
+            .then(function(body){
+                node.status({fill:"yellow",shape:"dot",text: "stock: " + body.stockLevel + " reorder: " + body.reorderLevel});
+                node.send({payload:body});
+            })
+            .catch(function(e){
+                console.error(e);
+            });
+        });
+    }
 
     function PaprAdd(config) {
         RED.nodes.createNode(this, config);
@@ -11,24 +47,12 @@ module.exports = function(RED) {
 
         node.yaasCustomerCredentials = RED.nodes.getNode(config.yaasCustomerCredentials);
         node.yaasCredentials = RED.nodes.getNode(config.yaasCredentials);
+        node.status({fill:"yellow",shape:"dot",text:"idle"});
 
         node.on('input', function(msg) {
-
-/*
-            var productdetails = (msg.payload.constructor === Array) ? msg.payload[0] : msg.payload;
-            var product = productdetails.product;
-            product.images = product.media;
-
-
-
-            var price = productdetails.prices[0];
-
-
-            var quantity = Math.round(node.quantity);
-            var currency = config.currency;
-            var siteCode = config.siteCode;
-*/           
-            var quantity = Math.round(node.quantity);
+          
+            var quantity = Math.round(node.quantity) || 1;
+            node.status({fill:"green",shape:"dot",text:"adding " + quantity});
             var customerToken;
 
             oauth2.getClientCredentialsToken(node.yaasCredentials.client_id, node.yaasCredentials.client_secret, [])
@@ -46,6 +70,7 @@ module.exports = function(RED) {
                 });
             })
             .then(function(body){
+                node.status({fill:"yellow",shape:"dot",text: "stock: " + body.stockLevel + " reorder: " + body.reorderLevel});
                 node.send({payload:body});
             })
             .catch(function(e){
@@ -60,8 +85,10 @@ module.exports = function(RED) {
 
         node.yaasCustomerCredentials = RED.nodes.getNode(config.yaasCustomerCredentials);
         node.yaasCredentials = RED.nodes.getNode(config.yaasCredentials);
+        node.status({fill:"yellow",shape:"dot",text:"idle"});
 
         node.on('input', function(msg) {
+            node.status({fill:"green",shape:"dot",text:"decreasing"});
             var customerToken;
 
             oauth2.getClientCredentialsToken(node.yaasCredentials.client_id, node.yaasCredentials.client_secret, [])
@@ -79,6 +106,7 @@ module.exports = function(RED) {
                 });
             })
             .then(function(body){
+                node.status({fill:"yellow",shape:"dot",text: "stock: " + body.quantity});
                 node.send({payload:body});
             })
             .catch(function(e){
@@ -87,6 +115,7 @@ module.exports = function(RED) {
         });
     }
 
+    RED.nodes.registerType('get papr stock', PaprGetStock);
     RED.nodes.registerType('papr add', PaprAdd);
     RED.nodes.registerType('papr decrease', PaprDecrease);
 };
