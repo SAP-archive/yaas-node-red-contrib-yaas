efe6f07d9ff5501acfba596a433c5e9377d5f30a
Lars Gregori <lars.gregori@sap.com>
2016-08-12 11:06:42 +0200

    coupon: first create and than get

--- coupon.js
+++ coupon.js
@@ -2,40 +2,6 @@ module.exports = function(RED) {
 
     var YaaS = require("yaas.js");
 
-    function YaasCouponGetNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
-
-        node.yaasCredentials = RED.nodes.getNode(config.yaasCredentials);
-        node.tenant_id = config.tenantId;
-
-        node.status({fill: "red", shape: "ring", text: "disconnected"});
-
-        var yaas = new YaaS();
-        yaas.init(node.yaasCredentials.client_id,
-            node.yaasCredentials.client_secret,
-            'hybris.coupon_redeem',
-            node.tenant_id)
-        .then(function() {
-          node.on("input",function(msg) {
-            var couponId = msg.payload.id || msg.payload;
-            node.status({fill: "green", shape: "dot", text: couponId});
-            console.log(couponId);
-            yaas.coupon.get(couponId)
-            .then(function(response) {
-              console.log(response);
-              node.status({fill: "yellow", shape: "dot", text: response.body.code + " (" + response.body.status + ")"});
-              node.send({payload: response.body});
-            }, console.error);
-          });
-        });
-
-        node.on('close', function() {});
-    }
-
-    RED.nodes.registerType("get", YaasCouponGetNode);
-
-
     function YaasCouponCreateNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
@@ -87,7 +53,37 @@ module.exports = function(RED) {
 
         node.on('close', function() {});
     }
-
     RED.nodes.registerType("create coupon", YaasCouponCreateNode);
 
+    function YaasCouponGetNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.yaasCredentials = RED.nodes.getNode(config.yaasCredentials);
+        node.tenant_id = config.tenantId;
+
+        node.status({fill: "red", shape: "ring", text: "disconnected"});
+
+        var yaas = new YaaS();
+        yaas.init(node.yaasCredentials.client_id,
+            node.yaasCredentials.client_secret,
+            'hybris.coupon_redeem',
+            node.tenant_id)
+        .then(function() {
+          node.on("input",function(msg) {
+            var couponId = msg.payload.id || msg.payload;
+            node.status({fill: "green", shape: "dot", text: couponId});
+            console.log(couponId);
+            yaas.coupon.get(couponId)
+            .then(function(response) {
+              console.log(response);
+              node.status({fill: "yellow", shape: "dot", text: response.body.code + " (" + response.body.status + ")"});
+              node.send({payload: response.body});
+            }, console.error);
+          });
+        });
+
+        node.on('close', function() {});
+    }
+    RED.nodes.registerType("get", YaasCouponGetNode);
 };
